c0ac6428ce870ab7f880da445642e0abef03c6a2
Oxshady <shadymahmod55@gmail.com>
2024-09-16 03:46:08 +0300

    done

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import JWTManager
 from datetime import timedelta
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = '7809b82167fe37c83af13486f17903a873ea5e9b4a244253'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=365)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=365 * 5)
 jwt = JWTManager(app)
 app.register_blueprint(api_v1)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
