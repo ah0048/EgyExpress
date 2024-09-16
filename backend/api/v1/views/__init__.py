5f0b7fc4791efec0d9b6e9a91169e41e862517d2
Oxshady <shadymahmod55@gmail.com>
2024-09-16 03:24:27 +0300

    ^_^ logic

--- backend/api/v1/views/__init__.py
+++ backend/api/v1/views/__init__.py
@@ -1,10 +1,7 @@
 from flask import Blueprint
 
-<<<<<<< HEAD
-api_v1 = Blueprint('api_v1', __name__, url_prefix='/api/')
-=======
+
 api_v1 = Blueprint('api_v1', __name__, url_prefix='/api')
->>>>>>> shadi_backend
 
 from api.v1.views.users import *
 from api.v1.views.orders import *
